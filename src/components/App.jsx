52854b91e286f1dd960b31ec37829a466a604bc7
Sergey Kuran <kuran.serg@gmail.com>
2023-07-14 17:41:07 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
-export const App = () => {
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { lazy } from 'react';
+
+import Layout from './Layout';
+
+const HomePage = lazy(() => import('./Pages/HomePage'));
+const Movies = lazy(() => import('./Pages/MoviesPage'));
+const MovieDetails = lazy(() => import('./MovieDetails'));
+const Cast = lazy(() => import('./Cast/'));
+const Reviews = lazy(() => import('./Reviews'));
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movies/:movieId" element={<MovieDetails />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </>
   );
 };
+export default App;
