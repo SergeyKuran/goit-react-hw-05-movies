2488a800a70f07c98e3b89abf93f0f5eddb1d05f
Sergey Kuran <kuran.serg@gmail.com>
2023-07-16 14:31:11 +0300

    Update

--- src/components/Pages/MoviesPage/Movies.jsx
+++ src/components/Pages/MoviesPage/Movies.jsx
@@ -1,33 +1,84 @@
-import { useState } from 'react';
-import { Container } from './Movies.styled';
+import { useEffect, useState } from 'react';
+import { useLocation, useSearchParams } from 'react-router-dom';
+
+import Loader from 'components/Loader';
+import params from 'api/api_key';
+
+import { A, Container, Form, Label, Li } from './Movies.styled';
 
 const Movies = () => {
-  const [seachText, setSeachText] = useState('');
+  const [images, setImages] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [seachParams, setSeachParams] = useSearchParams('');
+  const seachId = seachParams.get('seachId') ?? '';
+  const location = useLocation();
 
-  const onHangleChange = evt => {
-    setSeachText(evt.target.value);
-  };
+  useEffect(() => {
+    const savedResults = JSON.parse(localStorage.getItem('search'));
+    if (savedResults) {
+      setImages(savedResults);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (images.length === 0) {
+      return;
+    }
+
+    localStorage.setItem('search', JSON.stringify(images));
+  }, [images]);
 
   const onHandleSubmit = evt => {
     evt.preventDefault();
+
+    const fetchData = async () => {
+      try {
+        setIsLoading(true);
+        const response = await fetch(
+          `https://api.themoviedb.org/3/search/movie?query=${seachId}&${params}`
+        );
+        if (response.ok) {
+          const { results } = await response.json();
+          setImages(results);
+        } else {
+          throw new Error();
+        }
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
   };
 
   return (
     <Container>
-      <form
-        onSubmit={() => {
-          onHandleSubmit();
-        }}
-      >
-        <input
-          type="text"
-          value={seachText}
-          autoComplete="on"
-          placeholder="Please, enter seach movies"
-          onChange={onHangleChange}
-        />
-      </form>
-      Movies
+      <Form onSubmit={onHandleSubmit}>
+        <Label>
+          Movies
+          <input
+            type="text"
+            value={seachId}
+            autoComplete="on"
+            placeholder="Please, enter seach movies"
+            onChange={evt => setSeachParams({ seachId: evt.target.value })}
+          />
+        </Label>
+      </Form>
+      {isLoading && <Loader />}
+      {error && <Container>{error.message}</Container>}
+      <ul>
+        {images?.map(el => (
+          <Li key={el.id}>
+            <A to={`/movies/${el.id}`} state={{ from: location }}>
+              {el.title}
+            </A>
+          </Li>
+        ))}
+      </ul>
     </Container>
   );
 };
--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,5 +1,59 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+import Loader from 'components/Loader';
+import params from 'api/api_key';
+
+import { Container } from './Reviews.styled';
+
 const Reviews = () => {
-  return <>Reviews</>;
+  const [images, setImages] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const { movieId } = useParams();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setIsLoading(true);
+        const response = await fetch(
+          `https://api.themoviedb.org/3/movie/${movieId}/reviews?${params}`
+        );
+
+        if (response.ok) {
+          const { results } = await response.json();
+          setImages(results);
+        } else {
+          throw new Error();
+        }
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [movieId]);
+
+  return (
+    <>
+      {isLoading && <Loader />}
+      {error && <Container>{error.message}</Container>}
+      <ul>
+        {images.length === 0 ? (
+          <p>We don`t have many reviews for this movies.</p>
+        ) : (
+          images.map(({ id, author, content }) => (
+            <li key={id}>
+              <h3>Author: {author}</h3>
+              <p>{content}</p>
+            </li>
+          ))
+        )}
+      </ul>
+    </>
+  );
 };
 
 export default Reviews;
